e6f50620bca264931f6cc544448ca37fd90d8257
nathaniel <nathaniel@playstudios.com>
2017-03-12 14:58:10 -0700

    algorithm appears to be working soundly, now. Missing a counter example =(

--- mp-part.py
+++ mp-part.py
@@ -14,43 +14,51 @@ def forbidden_graphs(input_graph):
                 output_list.append(nbr)
                 output_list.append(G.neighbors(nbr)[1])
         break
-    #check if each node has a neighbor that has a neighbor
-    # if that's true, grab all three and return them in a list
     return output_list
 
-def monopolar_partition(input_graph, k):
+def monopolar_partition(input_graph):
     #initialization
     A_init = Graph() #an empty graph
     B_init = input_graph
+    k = input_graph.number_of_nodes()
     Q = [[A_init,B_init],] # deliberately double-depth Q to manage 'branches'
     #loop body
     for branch in Q:
-        A = branch.popleft()
+        A = branch.pop(0)
+        print "A "
+        print A.nodes()
         B = branch.pop()
-        if number_of_nodes(A) > k:
+        print "B "
+        print B.nodes()
+        if nx.number_of_nodes(A) > k:
             continue #reject branch
 
         p3s = forbidden_graphs(B)
-        if p3s == []: #this needs to change
+        print "p3s "
+        print p3s
+        if p3s == []:
             return 'yes'
             break
         else:
             for vertex in p3s:
-                A_new = A.append(vertex)
-                B_new = B.remove(vertex) # this decomposition isn't sufficient
-                                         # we must also kill it off in every value
-                                         # shit. some confusion here
-                                         # B is a list of vertices not a graph
+                # this decomposition is insufficient I need nx functions
+                A_new = nx.union(A,nx.subgraph(input_graph,vertex))
+                B_new = Graph(B)
+                B_new.remove_node(vertex)
                 Q.append([A_new,B_new])
 
         #all else fails
     return 'no'
 
 def main():
-    G = Graph()
-    G.add_nodes_from([1,2,3,4])
-    G.add_edges_from([(1,2),(2,3),(1,4)])
-    print forbidden_graphs(G)
+    N = Graph()
+    N.add_nodes_from([1,2,3,4])
+    N.add_edges_from([(1,2),(2,3),(4,1)])
+    Y = Graph()
+    Y.add_nodes_from([1,2,3,4,5,6,7])
+    Y.add_edges_from([(1,2),(2,3),(1,4),(4,3),(4,5),(4,6),(4,7),(1,3),(2,4)])
+    print monopolar_partition(N)
+    print monopolar_partition(Y)
 
 if __name__ == '__main__':
     main()
