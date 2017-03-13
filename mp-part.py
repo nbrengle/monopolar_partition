07b83e8f20fadb946eafcb49646db5e1078ecfa1
nathaniel <nathaniel@playstudios.com>
2017-03-12 23:47:40 -0700

    bowtie1 is broken, should return no; checkin prior to some surgery

--- mp-part.py
+++ mp-part.py
@@ -8,40 +8,55 @@ def forbidden_graphs(input_graph):
     output_list = []
     #loop through all the nodes
     for node in nx.nodes_iter(G): #not sure I need an iter here
+        #print "node " + str(node)
         for nbr in G.neighbors(node):
+            #print "nbr " + str(nbr)
+            #print G.neighbors(nbr)
             if len(G.neighbors(nbr)) > 1:
-                output_list.append(node)
-                output_list.append(nbr)
-                output_list.append(G.neighbors(nbr)[1])
-        break
+                neighborsneighbors = G.neighbors(nbr)
+                neighborsneighbors.remove(node)
+                for nbrnbr in neighborsneighbors:
+                    #print "nbrnbr " + str(nbrnbr)
+                    #print G.neighbors(nbrnbr)
+                    if node not in G.neighbors(nbrnbr):
+                        output_list.append(node)
+                        #print node
+                        output_list.append(nbr)
+                        #print nbr
+                        output_list.append(G.neighbors(nbr)[1])
+                        #print G.neighbors(nbr)[1]
+                        break
+                break
+            if output_list != []:
+                break
+        if output_list != []:
+            break
     return output_list
 
-def monopolar_partition(input_graph):
+def monopolar_partition(input_graph, k):
     #initialization
     A_init = Graph() #an empty graph
     B_init = input_graph
-    k = input_graph.number_of_nodes()
     Q = [[A_init,B_init],] # deliberately double-depth Q to manage 'branches'
     #loop body
     for branch in Q:
         A = branch.pop(0)
-        print "A "
-        print A.nodes()
+        #print "A "
+        #print A.nodes()
         B = branch.pop()
-        print "B "
-        print B.nodes()
+        #print "B "
+        #print B.nodes()
         if nx.number_of_nodes(A) > k:
             continue #reject branch
 
         p3s = forbidden_graphs(B)
-        print "p3s "
-        print p3s
+        #print "p3s "
+        #print p3s
         if p3s == []:
             return 'yes'
             break
         else:
             for vertex in p3s:
-                # this decomposition is insufficient I need nx functions
                 A_new = nx.union(A,nx.subgraph(input_graph,vertex))
                 B_new = Graph(B)
                 B_new.remove_node(vertex)
@@ -51,14 +66,26 @@ def monopolar_partition(input_graph):
     return 'no'
 
 def main():
-    N = Graph()
-    N.add_nodes_from([1,2,3,4])
-    N.add_edges_from([(1,2),(2,3),(4,1)])
-    Y = Graph()
-    Y.add_nodes_from([1,2,3,4,5,6,7])
-    Y.add_edges_from([(1,2),(2,3),(1,4),(4,3),(4,5),(4,6),(4,7),(1,3),(2,4)])
-    print monopolar_partition(N)
-    print monopolar_partition(Y)
+
+    G = Graph()
+    G.add_nodes_from([1,2,3,5,6,7])
+    G.add_edges_from([(1,2),(2,3),(3,5),(3,6),(6,7),(1,3)])
+    print "G:1 " + monopolar_partition(G,1)
+
+    k5 = nx.complete_graph(5)
+    G.add_nodes_from([5,6,7])
+    G.add_edges_from([(4,5),(4,6),(4,7)])
+    print "k5:1 "+ monopolar_partition(k5,1)
+
+    tri1 = nx.complete_graph(3)
+    tri2 = nx.complete_graph(3)
+    tri3 = nx.disjoint_union(tri1,tri2)
+    tri3.add_edge(2,3)
+    #print tri3.nodes()
+    #print tri3.edges()
+    print "bowtie:1 " + monopolar_partition(tri3,1)
+    print "bowtie:2 " + monopolar_partition(tri3,2)
+
 
 if __name__ == '__main__':
     main()
