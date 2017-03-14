b6db22e6866988714705d79876fbc85472a11a18
nathaniel <nathaniel@playstudios.com>
2017-03-13 19:58:53 -0700

    adds documentation for reduction and branching rules

--- ind-rec.py
+++ ind-rec.py
@@ -2,27 +2,53 @@ from networkx import Graph
 import networkx as nx
 
 def reduction_rule_5_1(constraint):
+    '''
+    If G[ACP] is not a cluster graph with at most k clusters,
+    or if G[BCP] is not an edgeless graph,
+    then reject the current constraint.
+    '''
     pass
     if len(constraint) != 4:
         raise ValueError("A constraint is a 4-tuple")
 
 def reduction_rule_5_2(constraint):
+    '''
+    If there is a vertex u ∈ BC∗ that has a neighbor in BCP,
+    then set ACP ← ACP ∪ {u} and BC∗ ← BC∗ \ {u};
+    ie: replace C with the constraint (AC∗, ACP ∪ {u}, BC∗ \ {u}, BCP)
+    '''
     pass
     if len(constraint) != 4:
         raise ValueError("A constraint is a 4-tuple")
 
-
 def reduction_rule_5_3(constraint):
+    '''
+    If there is a vertex u ∈ AC∗ and two vertices w, x ∈ ACP
+    such that G[{u, w, x}] is a P3, set AC∗ ← AC∗ \ {u}
+    and BCP ← BCP ∪ {u}.
+    '''
     pass
     if len(constraint) != 4:
         raise ValueError("A constraint is a 4-tuple")
 
 def branching_rule_5_1(constraint):
+    '''
+    If there are two vertices u, w ∈ AC∗ and a vertex x ∈ ACP
+    such that G[{u, w, x}] is a P3, then branch into two branches:
+    one associatedwith the constraint (AC∗ \ {u}, ACP, BC∗, BCP ∪ {u})
+    and one associated with (AC∗ \ {w}, ACP, BC∗, BCP ∪ {w}).
+    '''
     pass
     if len(constraint) != 4:
         raise ValueError("A constraint is a 4-tuple")
 
 def branching_rule_5_2(constraint):
+    '''
+    If there is a vertex u ∈ AC∗such that {u} is a clusterin G[A'],
+    then branch into two branches: the first is associated with the
+    constraint (AC∗ \ {u}, ACP ∪ {u}, BC∗, BCP),
+    and the second is associated with (AC∗ \ {u}, ACP, BC∗, BCP ∪ {u}).
+    '''
     pass
     if len(constraint) != 4:
         raise ValueError("A constraint is a 4-tuple")
